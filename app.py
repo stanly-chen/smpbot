99072674e1d4080f4b5c44732de98627c6e2d809
stanly-chen <34558979+stanly-chen@users.noreply.github.com>
2019-04-16 00:31:16 +0800

    test1

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
+
     # get request body as text
     body = request.get_data(as_text=True)
     print("Request body: " + body, "Signature: " + signature)
