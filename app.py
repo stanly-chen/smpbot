4edcd098964c7716a41ae6614a981983eea72c3c
stanly-chen <34558979+stanly-chen@users.noreply.github.com>
2019-04-26 00:47:51 +0800

    1

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def handle_message(event):
     elif "電視" in text:
         content = '已發送電視遙控器訊號'
         param = {'temp': '0', 'tv': '1Y1', 'wm': '0'}
-        requests.get('https://dweet.io/dweet/for/stanlykuasled', params=param)
+        requests.get('https://dweet.io/dweet/for/stanlykuasled2', params=param)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=content)
